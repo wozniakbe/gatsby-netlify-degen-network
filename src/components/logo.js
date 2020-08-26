d321357333b3dfeed25ae151316a31e287985ac2
Ben Wozniak <wozniakbe@msoe.edu>
2020-08-26 08:32:14 -0500

    trying to add logo to header

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
 
 const Logo = (props) => (
   <div className="site-logo">
-    <Link to="/">{props.title}</Link>
+    <Link to="/"><Img 
+              fluid={props.file.childImageSharp.fluid} 
+              alt={'ProAmPicks - Featured image'}
+              className="featured-image"
+            /></Link>
   </div>
 )
 
 export default Logo
+
+// export const pageQuery = graphql`
+//   query {
+//     fileName: file(relativePath: { eq: "assets/pro_am_picks.jpeg" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 400, maxHeight: 250) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+//   }
+// `
--- src/content/pages/index.md
+++ src/content/pages/index.md
@@ -9,3 +9,4 @@ cta:
   ctaLink: /about
 ---
 Offering professional picks for the amateur sports bettor
+<blockquote class="twitter-tweet" data-theme="light"><p lang="en" dir="ltr">5-0 day yesterday, let’s get back at it:<br><br>CHC ML -137<br>CWS RL -122<br>Blue Jays ML -137<br>Vegas -1 -109<br>Clippers/Cardinals ML parlay -108</p>&mdash; ProAmPicks (@ProAmPicks) <a href="https://twitter.com/ProAmPicks/status/1298299871909355521?ref_src=twsrc%5Etfw">August 25, 2020</a></blockquote>
