c338784c7785e3b411d2ee990cfd798a81bf395e
Ben Wozniak <wozniakbe@msoe.edu>
2020-08-31 19:37:31 -0500

    Fixed PWA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,18 +94,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-advanced-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `static/assets/stackrole.png`,
-      },
-    },
     'gatsby-plugin-offline',
   ],
 }
